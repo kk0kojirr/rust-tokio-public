11bac9b943695cb079a5816bce3e1cbc15b819ea
kk0kojirr <20761208+kk0kojirr@users.noreply.github.com>
2022-05-06 07:20:00 +0000

    add echoserver

---
+++ examples/echo-server-first.rs
@@ -0,0 +1,30 @@
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listner = TcpListener::bind("127.0.0.1:6142").await?;
+
+    loop {
+        let (mut socket, _) = listner.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = vec![0; 1024];
+
+            loop {
+                match socket.read(&mut buf).await {
+                    Ok(0) => return,
+                    Ok(n) => {
+                        // println!("{:?}", &buf);
+                        if socket.write_all(&buf[..n]).await.is_err() {
+                            return;
+                        }
+                    }
+                    Err(_) => {
+                        return;
+                    }
+                }
+            }
+        });
+    }
+}
